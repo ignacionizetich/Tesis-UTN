a619d6620f056ca5646b9e812d77a8ffc888cfa4
Ignacio Nizetich <68255407+INizetich@users.noreply.github.com>
2025-05-16 20:09:13 -0300

    update branches with backend changes
    * agregacion de validacion de que el usuario si o si verifique su cuenta para iniciar sesion
    
    * agregacion de validacion de que el usuario si o si verifique su cuenta para iniciar sesion 
    
    agregacion de validacion de que la cuenta del usuario este habilitada una vez verificando su cuenta via email
    
    * agregacion de validacion de que el usuario si o si verifique su cuenta para iniciar sesion
    
    agregacion de validacion de que la cuenta del usuario este habilitada una vez verificando su cuenta via email
    
    * backend
    
    * agregacion de metodos en account repository para encontrar una cuenta por alias o cvu
    
    * Backend changes to production
    
    * agregacion de validacion de que el usuario si o si verifique su cuenta para iniciar sesion
    
    agregacion de validacion de que la cuenta del usuario este habilitada una vez verificando su cuenta via email
    
    * backend
    
    * agregacion de metodos en account repository para encontrar una cuenta por alias o cvu
    
    * HOTFIX: arregle el crasheo de la app ya que inicializaba y crasheaba por un bean que no haciamos preDestroy

--- src/main/java/com/EDJ/ArCash/ArCashApplication.java
+++ src/main/java/com/EDJ/ArCash/ArCashApplication.java
@@ -1,10 +1,6 @@
 package com.EDJ.ArCash;
 
 
-import com.EDJ.ArCash.Models.Account;
-import com.EDJ.ArCash.Repository.AccountRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -12,7 +8,6 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 
 import java.util.Optional;
 import java.util.Scanner;
-
 
 /// ES POSIBLE BUILDEAR LA APP EN RAILWAY SIN DOCKER FILE. SOLO JAVA 17.
 /// https://arcash.ddns.net/
@@ -29,54 +24,10 @@ import java.util.Scanner;
 @SpringBootApplication
 public class ArCashApplication {
 
-    private final AccountRepository accountRepository;
-
-    private final  Scanner scanner = new Scanner(System.in);
-
-    public ArCashApplication(AccountRepository accountRepository) {
-        this.accountRepository = accountRepository;
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(ArCashApplication.class, args);
 
-
-
-
     }
 
-    @Bean
-    public CommandLineRunner commandLineRunner() {
-
-
-
-        System.out.println("Ingrese un alias o cvu de cuenta para transferir: ");
-        String aliascvu = scanner.nextLine();
-        Optional<Account> optional = accountRepository.findByAccountNickname(aliascvu);
-
-     if(optional.isPresent()){
-         System.out.println("CUENTA ENCONTRADA: ");
-         Account account = optional.get();
-         System.out.println(optional.get().getAccountCvu());
-         System.out.println(optional.get().getAccountNickname());
-         System.out.println(account.getUser().mostrarInformacion());
-
-     }else {
-
-         Optional<Account> optionalCvu = accountRepository.findByAccountCvu(aliascvu);
-
-         if(optionalCvu.isPresent()){
-             System.out.println("CUENTA ENCONTRADA: ");
-             Account account = optionalCvu.get();
-             System.out.println(optionalCvu.get().getAccountCvu());
-             System.out.println(optionalCvu.get().getAccountNickname());
-             System.out.println(account.getUser().mostrarInformacion());
-         }else {
-             System.out.println("No existe ninguna cuenta con ese alias o cvu");
-         }
-
-     }
-
-        return null;
-    }
 }
